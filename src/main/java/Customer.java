54933a25cd9e125ab81bb37ad38ab1dafbbfffec
Monesha <moneshadhali@gmail.com>
2024-02-08 16:32:17 +0000

    Customer class and test_class developed

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private double wallet;
+    private ArrayList<Artwork> artCollection;
+
+    public Customer(String name, double wallet){
+        this.name = name;
+        this.wallet = wallet;
+        this.artCollection = new ArrayList<Artwork>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public ArrayList<Artwork> getArtCollection() {
+        return artCollection;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void buyProduct(double price){
+        this.wallet -= price;
+    }
+
+    public void addToCollection(Artwork art){
+        this.artCollection.add(art);
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CustomerTest {
+
+    Customer customer;
+    Artist lora;
+    Artwork art1;
+    ArrayList<Artwork> tempArtCollection;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer ("James Bond", 1000);
+        lora = new Artist("Lora");
+        art1 = new Artwork("Flower", lora, 40);
+        tempArtCollection = new ArrayList<Artwork>();
+    }
+
+    @Test
+    public void canGetName(){
+        assertThat(customer.getName()).isEqualTo("James Bond");
+    }
+
+    @Test
+    public void canSetName(){
+        customer.setName("007");
+        assertThat(customer.getName()).isEqualTo("007");
+    }
+
+    @Test
+    public void canGetWallet(){
+        assertThat(customer.getWallet()).isEqualTo(1000);
+    }
+
+    @Test
+    public void canGetArtCollection(){
+        assertThat(customer.getArtCollection()).isEqualTo(tempArtCollection);
+    }
+
+    @Test
+    public void canBuyProduct(){
+        double price = art1.getPrice();
+        customer.buyProduct(price);
+        assertThat(customer.getWallet()).isEqualTo(960);
+    }
+
+    @Test
+    public void canAddToCollection(){
+        tempArtCollection.add(art1);
+        customer.addToCollection(art1);
+        System.out.println(customer);
+        assertThat(customer.getArtCollection()).isEqualTo(tempArtCollection);
+    }
 }
+
+
